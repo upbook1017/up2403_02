932ab8cdbbeed9e214b04d012c223ddf8a5b4845
upbook1017 <upbook1017@gmail.com>
2024-12-04 16:13:04 +0900

    24/12/4/マイグレーション及びモデル作成(topic)

---
+++ app/Models/Board.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Board extends Model
+{
+    use HasFactory;
+
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'title' => 'required',
+        'message' => 'required',
+    );
+
+    public function getData()
+    {
+        return $this->name . $this->title . $this->message;
+    }
+
+}
